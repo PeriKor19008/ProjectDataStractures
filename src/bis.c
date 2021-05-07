18e6083faa27dd407f88b65d64b03e088cf1e576
PeriKor <peri1908kor@gmail.com>
2021-05-07 14:12:47 +0300

    all first part complete

--- src/bis.c
+++ src/bis.c
@@ -3,39 +3,51 @@
 
 int bis(value* arr,int x,int low,int hi){
 
-    //we did not sort the array inside the function so tha we did not introduse more veriables
-    //to the analysis of the algorithm later
+
+   //if array small then do linear search
+
     int size=hi;
-    long M=low+((hi-low)*(x-arr[low].date)/(arr[hi].date-arr[low].date));
+    int M=low+((hi-low)*(x-arr[low].date)/(arr[hi].date-arr[low].date));
 
-   int i=1;
-    while (arr[M].volume!=x && i<sqrt(x)){
 
-       int m= sqrt(size)*i;
-       if(arr[M+m].date==x)
-           return arr[M+m].volume;
-       //if size of remaining array is small (3) we do a low cost linear search
-       if(hi-low==3){
-           for (int j=0;j<hi-low;j++){
-               if (arr[j].date==x)
-                   return arr[j].volume;
-           }
-           //failure to find any
-           return "not found";
-       }
-       //check if x is in the next or previous sqrt
-       //if it is in the next:
-       if (arr[M+m].date<x){
-           low=M;
-           hi=M+m;
-           bis(arr,x,low,hi);
-       }
-       if(M>=m && arr[M-m].date>x){
-           hi=M;
-           low=M-m;
-           bis(arr,x,low,hi);
-       }
-       i++;
+    while (arr[M].date!=x){
+        if (hi-low<=3){
+            for (int j=0;j<hi-low;j++){
+
+                if (arr[j].date==x)
+                    return arr[j].volume;
+            }
+            return -1;
+        }
+        int i=0;
+        int m= sqrt(size);
+
+        if(arr[M+m].date==x)
+            return arr[M+m].volume;
+        if(arr[M-m].date==x)
+            return arr[M-m].volume;
+
+
+
+        if(arr[M].date>x){
+            while(arr[M-(m*i)].date>=x){
+                i++;
+            }
+            low=M-(m*i);
+            hi=low+m;
+        }
+        if(arr[M].date<x){
+            while(arr[M+(m*i)].date<=x){
+                i++;
+            }
+            hi=M+(m*i);
+            low=hi-m;
+
+        }
+
+        M=low+((hi-low)*(x-arr[low].date)/(arr[hi].date-arr[low].date));
     }
-    return "not found";
+    if(arr[M].date==x)
+        return arr[M].volume;
+    return -1;
 }
